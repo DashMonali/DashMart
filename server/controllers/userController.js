6af051ed27ad798230f2b8587421a415e390dcda
MonaliDash <monalidash121@gmail.com>
2025-07-18 20:10:53 +0530

    frontend complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,6 +14,13 @@ export const register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = await User.create({ name, email, password: hashedPassword })
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.cookie('token', token, {
+            httpOnly: true,  //Prevent Js to access cookie
+            secure: process.env.NODE_ENV === 'production',  //Use secure cookies in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000,    //Cookie expiration time
+        });
     } catch (error) {
 
     }
